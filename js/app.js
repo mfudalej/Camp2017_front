e94ad8415d8e9a9d972f5536f57dd2f9180ac460
Mateusz Fudalej <mateusz.fudalej@gmail.com>
2017-08-02 14:02:04 +0200

    fix after pull

--- js/app.js
+++ js/app.js
@@ -4,3 +4,21 @@ $('.container').hide();
 $(document).ready(function() {
     $(".container").fadeIn(1000);
 });
+
+$('#password')
+    .keyboard({
+        openOn : null,
+        stayOpen : true,
+        layout : 'qwerty'
+    })
+    .addTyping();
+
+$('#password-opener').click(function(){
+    var kb = $('#password').getkeyboard();
+    // close the keyboard if the keyboard is visible and the button is clicked a second time
+    if ( kb.isOpen ) {
+        kb.close();
+    } else {
+        kb.reveal();
+    }
+});
